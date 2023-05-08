b704e45aeaea90a41ccb92757c3f38c14961ac58
Abrar <abrarmurad00@gmail.com>
2023-05-08 13:32:06 -0400

    added analytics and F&Q

--- src/components/Contact/ContactData.js
+++ src/components/Contact/ContactData.js
@@ -1,25 +1,25 @@
 export default [
     {
         id: 1,
-        question: "this",
-        answer: "lorem ipsum dolor sit amet",
+        question: "Do you do mobile?",
+        answer: "Yes, depending on the service and amount of cars",
     },
 
     {
         id: 2,
-        question: "is",
-        answer: "lorem ipsum dolor sit amet",
+        question: "Do you do ceramic coatings?",
+        answer: "Yes, price ranges depending on glass, rims, and trims.",
     },
 
     {
         id: 3,
-        question: "the",
-        answer: "lorem ipsum dolor sit amet",
+        question: "What are your hours?",
+        answer: "Opened from 8AM - 8PM on weekdays. On weekends, 9AM - 5PM.",
     },
 
     {
         id: 4,
-        question: "F&Q",
-        answer: "lorem ipsum dolor sit amet",
+        question: "Why should we choose you?",
+        answer: "5+ years of experience and a wide range of hyper/sport car owners that trust us with their vehicles to be maintained and looking spectacular.",
     },
 ];
